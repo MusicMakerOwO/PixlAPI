6376eaaa47f7c7c60b8e03c92505a2cf0288ff13
Music Maker <joshuanejdl@gmail.com>
2025-07-17 01:45:39 -0500

    fix: Added debounce to file edits + logs

--- Utils/GameList.js
+++ Utils/GameList.js
@@ -1,13 +1,16 @@
 const fs = require('fs');
 const path = require('path');
-const { GAMES_FOLDER } = require('../Constants.js');
+const { GAMES_FOLDER, ROOT_FOLDER } = require('../Constants.js');
 const FileWatcher = require('./FileWatcher.js');
+const { Log } = require('./Log.js');
+const Debounce = require('./Debounce.js');
 
 const GAMES = new Map(); // string -> { name, description, players: { min, max } }
 
 const Watcher = new FileWatcher(GAMES_FOLDER, true);
-Watcher.onChange = (filePath) => {
-	console.log(`Game file changed: ${filePath}`);
+Watcher.onChange = Debounce((filePath) => {
+	if (filePath.endsWith('~')) return; // Ignore temporary files
+	Log('DEBUG', `Reloading game file: ${filePath.replace(ROOT_FOLDER, '')}`);
 	delete require.cache[ filePath ];
 	const game = require(filePath);
 	GAMES.set(game.name, {
@@ -15,10 +18,11 @@ Watcher.onChange = (filePath) => {
 		description: game.description,
 		players: game.players
 	});
-}
-Watcher.onAdd = (filePath, type) => {
+}, 1000);
+Watcher.onAdd = Debounce((filePath, type) => {
 	if (type !== FileWatcher.FILE_TYPE.FILE) return;
-	console.log(`Game file added: ${filePath}`);
+	if (filePath.endsWith('~')) return; // Ignore temporary files
+	Log('DEBUG', `Adding game file: ${filePath.replace(ROOT_FOLDER, '')}`);
 	delete require.cache[ path.resolve(filePath) ];
 	const game = require(filePath);
 	GAMES.set(game.name, {
@@ -26,12 +30,13 @@ Watcher.onAdd = (filePath, type) => {
 		description: game.description,
 		players: game.players
 	});
-};
-Watcher.onRemove = (path) => {
-	console.log(`Game file removed: ${path}`);
+}, 1000);
+Watcher.onRemove = Debounce((path) => {
+	if (path.endsWith('~')) return; // Ignore temporary files
+	Log('DEBUG', `Removing game file: ${path.replace(ROOT_FOLDER, '')}`);
 	const gameName = path.split('/').pop().replace('.js', '');
 	GAMES.delete(gameName);
-};
+}, 1000);
 
 const gameFiles = fs.readdirSync(GAMES_FOLDER).filter(file => file.endsWith('.js'));
 gameFiles.forEach(file => {

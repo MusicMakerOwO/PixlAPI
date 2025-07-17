156701feb07ce1cd643dc2a9b2a0568dbe69233b
Music Maker <joshuanejdl@gmail.com>
2025-07-17 01:51:33 -0500

    feat: Added error checking for unknown games and full lobbies

--- Routes/Lobby/Create.js
+++ Routes/Lobby/Create.js
@@ -1,4 +1,5 @@
 const { CreateLobby } = require("../../Utils/LobbyUtils")
+const GameList = require("../../Utils/GameList");
 
 module.exports = {
 	method: 'POST',
@@ -12,6 +13,18 @@ module.exports = {
 		const game = req.body.game.trim();
 		const maxPlayers = Math.max(2, Math.min(100, parseInt(req.body.max_players)));
 		const privateLobby = !!req.body.private;
+
+		const gameData = GameList.get(game);
+		if (!gameData) {
+			return { status: 404, message: `Unknown game: ${game}` };
+		}
+
+		if (maxPlayers < gameData.players.min || maxPlayers > gameData.players.max) {
+			return {
+				status: 400,
+				message: `Invalid number of players - Must be between ${gameData.players.min} and ${gameData.players.max}`
+			};
+		}
 
 		const lobbyID = CreateLobby(game, maxPlayers, privateLobby);
 

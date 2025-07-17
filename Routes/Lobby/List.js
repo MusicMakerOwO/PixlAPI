050c8acee156621f70f8edf5fdaf354b1cad7797
Music Maker <joshuanejdl@gmail.com>
2025-07-17 01:46:57 -0500

    whitespace: Converted spaces to tabs for indentation

--- Routes/Lobby/List.js
+++ Routes/Lobby/List.js
@@ -1,14 +1,14 @@
 const { ListLobbies } = require('../../Utils/LobbyUtils');
 
 module.exports = {
-    method: 'GET',
-    route: '/lobby/list',
-    handler: async () => {
-        const lobbies = ListLobbies();
+	method: 'GET',
+	route: '/lobby/list',
+	handler: async () => {
+		const lobbies = ListLobbies();
 
-        return {
-            status: 200,
-            lobbies: lobbies
-        }
-    }
+		return {
+			status: 200,
+			lobbies: lobbies
+		}
+	}
 }

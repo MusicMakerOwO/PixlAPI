550e55e97e199872392ed0d4e53f97b3bf23f321
Music Maker <joshuanejdl@gmail.com>
2025-07-20 10:10:00 -0500

    style: Cleaning up comments

--- Utils/LobbyUtils.js
+++ Utils/LobbyUtils.js
@@ -1,31 +1,3 @@
-/*
-CREATE TABLE IF NOT EXISTS GameLobbies (
-	id TEXT NOT NULL PRIMARY KEY,
-
-	owner_id TEXT, -- NULL at creation, set to user_id directly after creation
-	game TEXT NOT NULL,
-	max_players INTEGER NOT NULL,
-
-	-- Private lobbies are not listed in the lobby list
-	-- You must have the exact code to join
-	private INTEGER NOT NULL DEFAULT 0,
-
-	in_progress INTEGER NOT NULL DEFAULT 0, -- cannot join lobbies after game starts
-	allow_spectators INTEGER NOT NULL DEFAULT 0, -- allow spectators to join after game starts
-
-	FOREIGN KEY (owner) REFERENCES Users(id) ON DELETE CASCADE
-) STRICT;
-
-CREATE TABLE IF NOT EXISTS Players (
-	lobby_id TEXT NOT NULL,
-	user_id TEXT NOT NULL UNIQUE, -- players can only be in one lobby at a time
-
-	PRIMARY KEY (lobby_id, user_id),
-	FOREIGN KEY (lobby_id) REFERENCES GameLobbies(id) ON DELETE CASCADE,
-	FOREIGN KEY (user_id) REFERENCES Users(user_id) ON DELETE CASCADE
-) STRICT;
-*/
-
 const Database = require("../Database");
 const GameList = require("./GameList");
 const GenerateCode = require("./GenerateCode");

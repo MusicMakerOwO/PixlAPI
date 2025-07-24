1a55c0a926080bd7b7d755605f3b21ec6a9db5de
Music Maker <joshuanejdl@gmail.com>
2025-07-24 11:20:41 -0500

    perf: No need to loop the entire class if game list is static

--- src/Routes/Games/List.ts
+++ src/Routes/Games/List.ts
@@ -1,13 +1,24 @@
 import GameList from '../../Utils/GameList';
 import {IEndpoint} from "../../types";
 
+// Simple form of memoization
+const PrettyGameList: Array<{ name: string, description: string, players: { min: number, max: number } }> = [];
+for (const gameName in GameList) {
+	const game = GameList.get(gameName)!;
+	PrettyGameList.push({
+		name: game.name,
+		description: game.description,
+		players: game.players
+	})
+}
+
 export default {
 	method: 'GET',
 	route: '/games/list',
 	handler: async () => {
 		return {
 			status: 200,
-			games: Object.fromEntries(GameList) // Map -> Record
+			games: PrettyGameList
 		}
 	}
 } as IEndpoint;

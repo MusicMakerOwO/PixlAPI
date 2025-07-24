0256a07156f03eb22c8fa6353b7744a9091bfaa4
Music Maker <joshuanejdl@gmail.com>
2025-07-24 11:23:29 -0500

    BREAKING CHANGE: Games are no longer dynamically loaded to help prevent type checking errors

--- src/Utils/GameList.ts
+++ src/Utils/GameList.ts
@@ -1,28 +1,9 @@
-import fs from 'node:fs';
-import {GAMES_FOLDER} from '../Constants.js';
-import {Log} from './Log.js';
+import {IGameClass} from '../types.js';
 
-const GAMES = new Map(); // string -> { name, description, players: { min, max } }
+import TicTacToe from '../Games/TicTacToe';
 
-const gameFiles = fs.readdirSync(GAMES_FOLDER).filter((file: string) => file.endsWith('.js'));
+const GAMES = new Map<string, IGameClass>();
 
-for (const file of gameFiles) {
-	const gamePath = `${GAMES_FOLDER}/${file}`;
-	try {
-		let game = require(gamePath) as { name: string, description: string, players: { min: number, max: number } } | { default: { name: string, description: string, players: { min: number, max: number } } };
-		if ('default' in game) {
-			// If the module exports a default, use that
-			game = game.default;
-		}
-		GAMES.set(game.name, {
-			name: game.name,
-			description: game.description,
-			players: game.players
-		});
-	} catch (error) {
-		// @ts-ignore
-		Log('ERROR', `Failed to load game from file "${file}": ${error.message}`);
-	}
-}
+GAMES.set(TicTacToe.name, TicTacToe);
 
 export default GAMES;
